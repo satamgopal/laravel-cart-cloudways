28b59185d35bb145bdb03a5c7caecd3c7bab7dd0
Gopal Satam <satamgopal@gmail.com>
2022-07-19 12:36:02 +0530

    Project code added

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function cart() {
+
+        //dd(session('cartItems'));
+
+        return view('cart.cart');
+    }
+
+    public function addToCart($id) {
+
+        $product = Product::FindOrFail($id);
+
+        $cartItems = session()->get('cartItems', []);
+
+        if(isset($cartItems[$id])) {
+            $cartItems[$id]['quantity']++;
+        } else {
+            $cartItems[$id] = [
+                "image_path" => $product->image_path,
+                "name" => $product->name,
+                "brand" => $product->brand,
+                "details" => $product->details,
+                "price" => $product->price,
+                "quantity" => 1
+            ];
+        }
+
+        session()->put('cartItems', $cartItems);
+
+        return redirect()->back()->with('success', 'Product added to cart!');
+
+    }
+
+    public function delete(Request $request) {
+
+        if($request->id) {
+            $cartItems = session()->get('cartItems');
+
+            if(isset($cartItems[$request->id])) {
+
+                unset($cartItems[$request->id]);
+
+                session()->put('cartItems', $cartItems);
+
+            }
+
+            return redirect()->back()->with('success', 'Product deleted successfully');
+
+        }
+
+    }
+
+    Public function update(Request $request) {
+
+        //dd($request->quantity);
+        if($request->id && $request->quantity) {
+
+            $cartItems = session()->get('cartItems');
+            $cartItems[$request->id]['quantity'] = $request->quantity;
+            session()->put('cartItems', $cartItems);
+
+        }
+
+        return redirect()->back()->with('success', 'Product added to cart!');
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\PagesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PagesController::class, 'index'])->name('home');
+
+Route::get('/shop', [ProductController::class, 'index'])->name('shop');
+Route::get('/shop/{id}', [ProductController::class, 'show'])->name('product');
+
+Route::get('/cart', [CartController::class, 'cart'])->name('cart');
+
+Route::get('/add-to-cart/{id}', [CartController::class, 'addToCart'])->name('add.to.cart');
+Route::get('/delete-from-cart/{id}', [CartController::class, 'delete'])->name('delete.from.cart');
+Route::put('/update-from-cart/{id}', [CartController::class, 'update'])->name('update.from.cart');
